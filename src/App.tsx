a3a495d86a591b3594c61f1fac02b52fe400de96
Rory Jarrard <rjarrard@gmail.com>
2024-10-23 15:45:31 -0600

    add place state

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
+import { useState } from 'react';
 import LocationSearch from './components/LocationSearch';
 import Map from './components/Map';
 import type { Place } from './api/Place';
 
 function App() {
+  const [place, setPlace] = useState<Place | null>(null);
+
   return (
     <div className="h-screen w-screen grid grid-cols-12">
       <div className="col-span-3 p-2">
         <LocationSearch />
       </div>
       <div className="col-span-9">
-        <Map />
+        <Map place={place} />
       </div>
     </div>
   )
