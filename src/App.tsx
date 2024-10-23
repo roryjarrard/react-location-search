891967a1db8d1abc0a282a000c59e5289e939086
Rory Jarrard <rjarrard@gmail.com>
2024-10-23 15:30:27 -0600

    add mvp components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import LocationSearch from './components/LocationSearch';
+import Map from './components/Map';
 
 function App() {
   return (
-    <div>
-      <h1>React TypeScript App</h1>
+    <div className="h-screen w-screen grid grid-cols-12">
+      <div className="col-span-3 p-2">
+        <LocationSearch />
+      </div>
+      <div className="col-span-9">
+        <Map />
+      </div>
     </div>
   )
 }
---
+++ src/components/LocationSearch.tsx
@@ -0,0 +1,7 @@
+export default function LocationSearch() {
+  return (
+    <div>
+      <h1>Location Search</h1>
+    </div>
+  )
+}
