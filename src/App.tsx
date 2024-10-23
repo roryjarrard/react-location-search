18698ec9f80095f9c39cbe5a3964d248cc6492fd
Rory Jarrard <rjarrard@gmail.com>
2024-10-23 15:51:16 -0600

    define place setter prop

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="h-screen w-screen grid grid-cols-12">
       <div className="col-span-3 p-2">
-        <LocationSearch />
+        <LocationSearch onPlaceClick={(p: Place) => setPlace(p)} />
       </div>
       <div className="col-span-9">
         <Map place={place} />
